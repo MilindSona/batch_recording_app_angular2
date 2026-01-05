8db53a62389e6d41854a9c88e240bbbd6891de87
Milind Sonawane <milind.g.sonawane@avanade.com>
2026-01-05 21:03:10 +0530

    first commit login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Bachmaster } from './pages/bachmaster/bachmaster';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'admin',
+    component: Layout,
+    children: [
+      {
+        path: 'batchmaster',
+        component: Bachmaster
+      },
+      {
+        path:'dashboard',
+        component:Dashboard
+      }
+    ]
+  }
+];
---
+++ src/app/pages/bachmaster/bachmaster.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bachmaster',
+  imports: [],
+  templateUrl: './bachmaster.html',
+  styleUrl: './bachmaster.css',
+})
+export class Bachmaster {
+
+}
