886853703eaa8c800d78fee4a84784a77522e529
Milind Sonawane <milind.g.sonawane@avanade.com>
2026-01-06 21:00:55 +0530

    6th jan 9pm

---
+++ src/app/constants/global.constant.ts
@@ -0,0 +1,3 @@
+export const GLOBAL_CONSTANT = {
+    LOCAL_KEY_LOGIN: 'batchuser'
+}
---
+++ src/app/core/guard/auth-guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { GLOBAL_CONSTANT } from '../../constants/global.constant';
+
+//Upto angular v15 services were used for guards
+//From angular v16 we can use functions for guards
+export const authGuard: CanActivateFn = (route, state) => {
+  debugger;
+  const router=inject(Router);
+  const localData = localStorage.getItem(GLOBAL_CONSTANT.LOCAL_KEY_LOGIN);
+  if(localData!=null){
+    return true
+  }else{
+    router.navigateByUrl('login');
+    return false;
+  }
+  
+};
